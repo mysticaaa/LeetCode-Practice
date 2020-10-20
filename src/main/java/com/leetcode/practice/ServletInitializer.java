e7afe9698c8066ea355dafa0ae3f0626285db9ae
wangjingyi <mysticaaa@163.com>
2020-10-20 15:03:23 +0800

    init project

---
+++ src/main/java/com/leetcode/practice/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.leetcode.practice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer
+{
+    
+    @Override
+    protected SpringApplicationBuilder configure(
+        SpringApplicationBuilder application)
+    {
+        return application.sources(PracticeApplication.class);
+    }
+    
+}
---
+++ src/test/java/com/leetcode/practice/PracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.leetcode.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
